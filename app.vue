21f5244eaadaee0a29e9c190358a5996e053fcec
P.E. Butler III <p.e.butler3@gmail.com>
2023-11-01 23:23:06 -0600

    feat: useBooks with books from api

---
+++ app.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const storeBooks = useBooks();
+
+const { data } = await useFetch('/api/books');
+const { books } = (data.value as { data: { books: Book[] } }).data;
+
+storeBooks.value = await books;
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style lang="scss">
+@use '@/assets/colors' as color;
+@use '@/assets/variables' as var;
+
+body {
+  color: color.$charcoal;
+  font-family: var.$globalFont;
+  margin: 0;
+}
+</style>
